17d35b257c671ae692fd997bc90c5129a999e1ae
Carolina Oliveira <carolinaoliveira@id.uff.br>
2020-11-19 14:07:18 -0300

    Impressão do arquivo

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from csv import reader
+import csv
 from sys import exit
 
+with open('notas.csv', 'r', encoding='utf-8') as arquivo_csv:
+    csv_reader = csv.DictReader(arquivo_csv, delimiter = ',')
+    for linha in csv_reader:
+        print(linha)
+
 class Estudante:
-    pass
+    def __init__(self, matricula, disciplina, curso, nota, ch, ano_semestre):
+        self._matricula = matricula
+        self._dicionario = {'disciplina' : [disciplina], 'curso' : [curso], 'nota' : [nota],
+                            'ch' : [ch], 'ano_semestre' : [ano_semestre]}
+        return
 
 class Container:
     pass
